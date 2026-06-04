6379cb6612dfce45f7b4b9fd4f89a70394f130d0
HJQ1128 <huangjiqiang1128@gmail.com>
2026-06-05 04:09:19 +0800

    1

--- src/components/MessageBoard.tsx
+++ src/components/MessageBoard.tsx
@@ -75,7 +75,7 @@ export default function MessageBoard() {
   };
 
   const handleDelete = async (id: string) => {
-    if (confirm('啥要删掉•︡ᯅ•︠ ？')) {
+    if (confirm('为啥要删掉•︡ᯅ•︠ ？')) {
       await remove(ref(db, `love_messages/${id}`));
     }
   };
